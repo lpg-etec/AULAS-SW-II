2632b283753a69d79bee05c2fc21bd9dafa61d2e
lpg-etec <luiggy.garcia@etec.sp.gov.br>
2025-04-28 19:28:56 -0300

    aula8

---
+++ aula-08/api.php
@@ -0,0 +1,30 @@
+<?php
+header("Content-Type: application/json");
+
+$usuarios = json_decode(file_get_contents('./usuarios.json'));
+
+switch ($_SERVER['REQUEST_METHOD']) {
+    case 'GET':
+        echo json_encode($usuarios);
+        break;
+    case 'POST':
+        $d = json_decode(file_get_contents('php://input'), true);
+        if (!isset($d['id']) || !isset($d['name']) || !isset($d['email'])) {
+            http_response_code(400);
+            echo json_encode(['erro' => 'Dados incompletos.']);
+            exit;
+        }
+        $id = max(array_column($usuarios, 'id'))+1;
+        $usuarios[] = [
+            'id' => $id,
+            'nome' => $d['nome'],
+            'email' => $d['email']
+        ];
+        file_put_contents('./usuarios.json', json_encode($usuarios));
+        break;
+    default:
+        http_response_code(405);
+        echo json_encode(['erro' => 'Método não encontrado.'])
+        break;
+}
+?>
